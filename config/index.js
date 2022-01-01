13c18917ed3f68df7462aa2584ffa3f7fd2313f5
Handsome-L <2813599930@qq.com>
2022-01-01 12:16:15 +0800

    初始文件准备

---
+++ config/index.js
@@ -0,0 +1,10 @@
+module.exports = {
+  // 项目配置
+  app: {
+    // node 环境变量方式
+    port: process.env.PORT || 3000
+    // 配置完成后命令行设置端口：set PORT=5656(cmd) $env:PORT=5656(powershell)
+  },
+  // 数据库配置
+  db: {}
+}
